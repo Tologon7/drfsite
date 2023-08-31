08967241e21e489f4609b28ecf74d9d1e53e28db
Tologon7 <marmokk@icloud.com>
2023-08-31 17:47:42 +0600

    first commit

---
+++ women/views.py
@@ -0,0 +1,34 @@
+from django.forms import model_to_dict
+from rest_framework import generics, viewsets, mixins
+from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+
+from .models import Women, Category
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from .serializers import WomenSerializer
+from women.pagination import PaginationWomen
+
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    pagination_class = PaginationWomen
+    # permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication, )
+
+
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly, )
